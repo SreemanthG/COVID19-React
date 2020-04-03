c0a1a595bf833f4f2640d9d5025730bfa9232e60
sreemanth <sreemanth2001@gmail.com>
2020-04-04 01:10:54 +0530

    Mapview Update-1

--- src/Components/Maps.js
+++ src/Components/Maps.js
@@ -1,6 +1,6 @@
-import React,{ useState,useEffect } from 'react';
+import React,{ Component } from 'react';
 import axios from 'axios';
-import ReactDOM from 'react-dom';
+
 
 export default class Maps extends React.Component {
 
@@ -8,39 +8,84 @@ export default class Maps extends React.Component {
     super(props);
     this.state = {
       count:[],
-      isHovering:false,
+      isMount:false,
       stateName: ""
     };
-    this.hoverUpdate = this.hoverUpdate.bind(this);
+
+    // this.check = this.check.bind( this);
+    // this.hoverUpdate = this.hoverUpdate.bind(this);
+
   }
 
 
-hoverUpdate = (e)=>{
+// hoverUpdate = (e)=>{
+//     // this.hoverUpdate = this.hoverUpdate.bind(this);
+//   e.preventDefault();
 //  var user= e.target.getAttribute("name");
-//   this.setState({
-//     stateName: user,
-//     isHovering: true,
+//  axios.get('https://covid19-india-adhikansh.herokuapp.com/state/'+user).then(res=>{
+//   this.setState({count : res.data.data,isHovering : true,stateName : user},() => {
+//     // only now the state was updated
+//     console.log("Data is here", this.state); 
 //   })
-//   console.log(this.state);  
+// });
+//  console.log(this.state)
+// }
 
-}
+// hoverUpdate = (e)=>{
+//   this.check(e.target.getAttribute("name"));
+// }
+
 componentDidMount(){
-  axios.get('https://covid19-india-adhikansh.herokuapp.com/state/Telengana').then(res=>{
-    this.setState({count : res.data.data })
-    console.log(this.state);  
-
+  // this.setState({stateName:this.props.name});
+  this.setState({stateName:this.props.name},()=>{
+    console.log(this.state)
   });
-  // this.hoverUpdate();
-
+  axios.get('https://covid19-india-adhikansh.herokuapp.com/state/'+this.state.stateName).then(res=>{
+    this.setState({count : res.data.data,stateName : this.props.name});
+console.log(this.state)
+})
 }
 
+componentDidUpdate(prevProps, prevState){
+  // if(this.state.count){
+  axios.get('https://covid19-india-adhikansh.herokuapp.com/state/'+this.state.stateName).then(res=>{
+    this.setState({count : res.data.data,stateName : this.props.name});
+    console.log(this.state.count)
+})
+  // }
+}
 
 
 render(){
   return(
+    <div>
     <ul>
-    {/* {this.state.count.name ? <p>StateName: {this.state.count.name}</p>: <p>Hover over the state</p>} */}
-  </ul>
+    {this.state.stateName ? <p>StateName: {this.state.stateName}</p>: <p>Hover over the state</p>}
+    </ul>
+
+     <div className="card1" id="test">
+           <div  className="stats1 c1col">
+               <span>Confirmed</span>
+               <span id="confirmed">{this.state.stateName}</span>
+           </div>
+           <div  className="stats1 c2col">
+               <span>Active</span>
+               <span id="active">{this.state.stateName}</span>
+           </div>
+           <div  className="stats1 c3col">
+               <span>Recovered</span>
+               <span id="recovered">{this.state.stateName}</span>
+           </div>
+           <div  className="stats1 c4col">
+               <span>Deceased</span>
+               <span id="deceased">{this.state.stateName}</span>
+           </div>
+       </div>
+       </div>
   )
 }
+
 }
+
+
+
